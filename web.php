5de79126ef6815216d0aea7e23398e489263bec1
Bong Cosca <bong.cosca@yahoo.com>
2015-01-30 08:27:54 +0800

    Exempt whitespace between % and succeeding operator from being minified (bcosca/fatfree#773)

--- web.php
+++ web.php
@@ -641,7 +641,7 @@ class Web extends Prefab {
 							if (ctype_space($src[$ptr])) {
 								if ($ptr+1<strlen($src) &&
 									preg_match('/[\w'.($ext[0]=='css'?
-										'#\.+\-*()\[\]':'\$').']{2}|'.
+										'#\.%+\-*()\[\]':'\$').']{2}|'.
 										'[+\-]{2}/',
 										substr($data,-1).$src[$ptr+1]))
 									$data.=' ';
