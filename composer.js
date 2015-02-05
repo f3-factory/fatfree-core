8bb7d664416cf25d7a0dff756cd54883f2fab266
Sascha Ohms <sasch9r@gmail.com>
2015-02-05 11:22:03 +0100

    whoopsie

--- composer.js
+++ composer.js
@@ -1,5 +1,5 @@
 {
-	"name": "bcosca/fatfree",
+	"name": "bcosca/fatfree-core",
 	"description": "A powerful yet easy-to-use PHP micro-framework designed to help you build dynamic and robust Web applications - fast!",
 	"homepage": "http://fatfreeframework.com/",
 	"license": "GPL-3.0",
