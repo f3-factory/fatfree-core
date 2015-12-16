7dd587f1cdc5b357e1dc2e20bd76b05406a4cf40
xfra35 <xfra35@gmail.com>
2015-12-16 17:38:41 +0100

    Bugfix in Image->resize: round dimensions instead of cast

--- image.php
+++ image.php
@@ -230,9 +230,9 @@ class Image {
 		$ratio=($origw=imagesx($this->data))/($origh=imagesy($this->data));
 		if (!$crop) {
 			if ($width/$ratio<=$height)
-				$height=$width/$ratio;
+				$height=round($width/$ratio);
 			else
-				$width=$height*$ratio;
+				$width=round($height*$ratio);
 		}
 		if (!$enlarge) {
 			$width=min($origw,$width);
@@ -245,12 +245,12 @@ class Image {
 		// Resize
 		if ($crop) {
 			if ($width/$ratio<=$height) {
-				$cropw=$origh*$width/$height;
+				$cropw=round($origh*$width/$height);
 				imagecopyresampled($tmp,$this->data,
 					0,0,($origw-$cropw)/2,0,$width,$height,$cropw,$origh);
 			}
 			else {
-				$croph=$origw*$height/$width;
+				$croph=round($origw*$height/$width);
 				imagecopyresampled($tmp,$this->data,
 					0,0,0,($origh-$croph)/2,$width,$height,$origw,$croph);
 			}
