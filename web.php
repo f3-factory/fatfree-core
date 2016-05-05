fdcc7870124d552ca22bd52232de55b9d91f7828
Bong Cosca <bong.cosca@yahoo.com>
2016-05-05 18:35:41 +0800

    Vulnerability fix

--- web.php
+++ web.php
@@ -565,7 +565,8 @@ class Web extends Prefab {
 			$path=$fw->get('UI').';./';
 		foreach ($fw->split($path,FALSE) as $dir)
 			foreach ($files as $file)
-				if (is_file($save=$fw->fixslashes($dir.$file))) {
+				if (is_file($save=$fw->fixslashes($dir.$file)) &&
+					is_bool(strpos($save,'../'))) {
 					if ($fw->get('CACHE') &&
 						($cached=$cache->exists(
 							$hash=$fw->hash($save).'.'.$ext[0],$data)) &&
@@ -674,6 +675,8 @@ class Web extends Prefab {
 						$dst.=$data;
 					}
 				}
+				else
+					file_put_contents('x.log',$dir.$file."\n",FILE_APPEND);
 		if (PHP_SAPI!='cli' && $header)
 			header('Content-Type: '.$mime.'; charset='.$fw->get('ENCODING'));
 		return $dst;
