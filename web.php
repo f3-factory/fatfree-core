0dedf26c1b24ebebc9ae3ff244eea7f0b7cf074f
ikkez <ikkez0n3@gmail.com>
2016-10-18 15:45:01 +0200

    fix deprecated CURLOPT_SSL_VERIFYHOST value value `1` was removed with curl 7.28.1 https://curl.haxx.se/mail/archive-2012-11/0028.html

--- web.php
+++ web.php
@@ -291,7 +291,7 @@ class Web extends Prefab {
 				return strlen($line);
 			}
 		);
-		curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,TRUE);
+		curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,2);
 		curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,FALSE);
 		ob_start();
 		curl_exec($curl);
