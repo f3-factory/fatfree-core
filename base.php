1cfdfccc0d04b868f2408345f3962d43fb4e89f8
Chris Fasel <crypto.scythe@gmail.com>
2016-11-12 14:42:52 +0100

    Correct handling of TTL for Redis
    If TTL is 0 the 'ex' flag is not setting the key as it would instantly expire. To create a key without TTL we need to omit the 'ex' flag.

--- base.php
+++ base.php
@@ -2417,7 +2417,11 @@ class Cache extends Prefab {
 			case 'apcu':
 				return apc_store($ndx,$data,$ttl);
 			case 'redis':
-				return $this->ref->set($ndx,$data,['ex'=>$ttl]);
+				if($ttl > 0) {
+					return $this->ref->set($ndx,$data,array('ex'=>$ttl));
+				} else {
+					return $this->ref->set($ndx,$data);
+				}
 			case 'memcache':
 				return memcache_set($this->ref,$ndx,$data,0,$ttl);
 			case 'wincache':
