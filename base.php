5c299e30c8f484baab1b3c497db793ab279ef657
Bong Cosca <bong.cosca@yahoo.com>
2017-03-10 04:15:22 +0800

    Remove built-in function support

--- base.php
+++ base.php
@@ -2775,8 +2775,7 @@ class Preview extends View {
 			$str,$parts)) {
 			$str=trim($parts[1]);
 			foreach (Base::instance()->split($parts[2]) as $func)
-				$str=function_exists($cmd=$func) ||
-					is_string($cmd=$this->filter($func))?
+				$str=is_string($cmd=$this->filter($func))?
 					$cmd.'('.$str.')':
 					'\Base::instance()->call('.
 						'$this->filter(\''.$func.'\'),['.$str.'])';
