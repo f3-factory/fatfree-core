4de5210da2f57988b6cedd4d6e385d0fc90c6430
ikkez <ikkez0n3@gmail.com>
2018-12-27 11:31:30 +0100

    3.6.6-dev baseline

--- base.php
+++ base.php
@@ -45,7 +45,7 @@ final class Base extends Prefab implements ArrayAccess {
 	//@{ Framework details
 	const
 		PACKAGE='Fat-Free Framework',
-		VERSION='3.6.5-Release';
+		VERSION='3.6.6-Dev';
 	//@}
 
 	//@{ HTTP status codes (RFC 2616)
