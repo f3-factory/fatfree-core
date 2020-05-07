e9f85720261c21fd5601a36bfb99d7b72dcda3ed
ikkez <ikkez0n3@gmail.com>
2020-05-07 21:07:06 +0200

    fix empty ID after insert, bcosca/fatfree#1175

--- db/sql/mapper.php
+++ db/sql/mapper.php
@@ -447,7 +447,8 @@ class Mapper extends \DB\Cursor {
 			if ($field['pkey']) {
 				$field['previous']=$field['value'];
 				if (!$inc && $field['pdo_type']==\PDO::PARAM_INT &&
-					is_null($field['value']) && !$field['nullable'])
+					empty($field['value']) && !$field['nullable'] &&
+					is_null($field['default']))
 					$inc=$key;
 				$filter.=($filter?' AND ':'').$this->db->quotekey($key).'=?';
 				$nkeys[$nctr+1]=[$field['value'],$field['pdo_type']];
