d03a6f74280532f855a39fd5872d0ef3f9c821d9
ikkez <ikkez0n3@gmail.com>
2022-02-05 16:47:28 +0100

    fix: plural format syntax with empty param, fixes #325

--- base.php
+++ base.php
@@ -972,7 +972,7 @@ final class Base extends Prefab implements ArrayAccess {
 					switch ($type) {
 						case 'plural':
 							preg_match_all('/(?<tag>\w+)'.
-								'(?:\s*\{\s*(?<data>.+?)\s*\})/',
+								'(?:\s*\{\s*(?<data>.*?)\s*\})/',
 								$mod,$matches,PREG_SET_ORDER);
 							$ord=['zero','one','two'];
 							foreach ($matches as $match) {
