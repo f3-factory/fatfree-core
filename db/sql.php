4d3adc600cfbb537e624c760295485535c2abd84
Ivan Penchev <ivanvpenchev@gmail.com>
2022-02-16 17:41:17 +0200

    Fix primary key detection with MSSQL and dbib.

--- db/sql.php
+++ db/sql.php
@@ -412,7 +412,7 @@ class SQL {
 						foreach ($conv as $regex=>$type)
 							if (preg_match('/'.$regex.'/i',$row[$val[2]]))
 								break;
-						if (!isset($rows[$row[$val[1]]])) // handle duplicate rows in PgSQL
+						if ($this->engine === 'dblib' || !isset($rows[$row[$val[1]]])) // handle duplicate rows in PgSQL
 							$rows[$row[$val[1]]]=[
 								'type'=>$row[$val[2]],
 								'pdo_type'=>$type,
