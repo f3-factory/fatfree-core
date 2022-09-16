a2f9832afaf3101032de376f69b88a4ec677eff7
n0nag0n <n0nag0n@users.noreply.github.com>
2022-09-16 13:18:02 -0600

    let jig handle utf8 issues a little more gracefully

--- db/jig.php
+++ db/jig.php
@@ -84,7 +84,10 @@ class Jig {
 		$fw=\Base::instance();
 		switch ($this->format) {
 			case self::FORMAT_JSON:
-				$out=json_encode($data,JSON_PRETTY_PRINT);
+				if(version_compare(PHP_VERSION, '7.2.0') >= 0)
+					$out=json_encode($data,JSON_INVALID_UTF8_IGNORE);
+				else
+					$out=json_encode($data,JSON_PARTIAL_OUTPUT_ON_ERROR);
 				break;
 			case self::FORMAT_Serialized:
 				$out=$fw->serialize($data);
