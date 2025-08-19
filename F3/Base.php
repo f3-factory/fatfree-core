b42182e5c354c6dc101272692e19f4521c544ebc
ikkez <ikkez0n3@gmail.com>
2025-08-19 23:09:08 +0200

    refactor: renovate UTF plugin

--- F3/Base.php
+++ F3/Base.php
@@ -17,6 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with Fat-Free Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author 2009-2019 Bong Cosca
  */
 
 namespace F3 {
