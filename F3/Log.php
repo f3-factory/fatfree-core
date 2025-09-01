a11cab1060d6b8f49405182f4fc2e3c0d22789b8
ikkez <ikkez0n3@gmail.com>
2025-09-02 01:00:42 +0200

    renovate more plugins

--- F3/Log.php
+++ F3/Log.php
@@ -1,62 +1,61 @@
 <?php
 
-/*
-
-	Copyright (c) 2009-2019 F3::Factory/Bong Cosca, All rights reserved.
-
-	This file is part of the Fat-Free Framework (http://fatfreeframework.com).
-
-	This is free software: you can redistribute it and/or modify it under the
-	terms of the GNU General Public License as published by the Free Software
-	Foundation, either version 3 of the License, or later.
-
-	Fat-Free Framework is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	General Public License for more details.
-
-	You should have received a copy of the GNU General Public License along
-	with Fat-Free Framework.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+/**
+ *
+ * Copyright (c) 2025 F3::Factory, All rights reserved.
+ *
+ * This file is part of the Fat-Free Framework (http://fatfreeframework.com).
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or later.
+ *
+ * Fat-Free Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Fat-Free Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace F3;
 
 /**
  * Custom logger
  */
-class Log {
+class Log
+{
 
     /** File name */
     protected string $file;
 
     /**
      * Write specified text to log file
-     * @param string $text
-     * @param string $format
      */
-    function write(string $text, string $format = 'r'): void
+    public function write(string $text, string $dateFormat = 'r'): void
     {
         $fw = Base::instance();
         $ip = isset($fw->SERVER['REMOTE_ADDR'])
             ? (' ['.$fw->SERVER['REMOTE_ADDR'].
-                (($fwd = \filter_var($fw->get('HEADERS.X-Forwarded-For'),
-                    FILTER_VALIDATE_IP))?(' ('.$fwd.')'):'')
+                (($fwd = \filter_var(
+                    $fw->get('HEADERS.X-Forwarded-For'),
+                    FILTER_VALIDATE_IP,
+                )) ? (' ('.$fwd.')') : '')
                 .']')
-            :'';
+            : '';
         foreach (\preg_split('/\r?\n|\r/', \trim($text)) as $line)
             $fw->write(
                 $this->file,
-                \date($format).$ip.' '.\trim($line).PHP_EOL,
-                TRUE
+                \date($dateFormat).$ip.' '.\trim($line).PHP_EOL,
+                true,
             );
     }
 
     /**
      * Erase log file
-     * @return void
      */
-    function erase(): void
+    public function erase(): void
     {
         @\unlink($this->file);
     }
@@ -64,11 +63,11 @@ class Log {
     /**
      * @param string $file filepath within LOGS dir
      */
-    function __construct(string $file)
+    public function __construct(string $file)
     {
         $fw = Base::instance();
         if (!\is_dir($dir = $fw->LOGS))
-            \mkdir($dir,Base::MODE,TRUE);
+            \mkdir($dir, Base::MODE, true);
         $this->file = $dir.$file;
     }
 
--- F3/Test.php
+++ F3/Test.php
@@ -1,100 +1,94 @@
 <?php
 
-/*
-
-	Copyright (c) 2009-2019 F3::Factory/Bong Cosca, All rights reserved.
-
-	This file is part of the Fat-Free Framework (http://fatfreeframework.com).
-
-	This is free software: you can redistribute it and/or modify it under the
-	terms of the GNU General Public License as published by the Free Software
-	Foundation, either version 3 of the License, or later.
-
-	Fat-Free Framework is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	General Public License for more details.
-
-	You should have received a copy of the GNU General Public License along
-	with Fat-Free Framework.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+/**
+ *
+ * Copyright (c) 2025 F3::Factory, All rights reserved.
+ *
+ * This file is part of the Fat-Free Framework (http://fatfreeframework.com).
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or later.
+ *
+ * Fat-Free Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Fat-Free Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace F3;
 
-//! Unit test kit
-class Test {
+/**
+ * Unit test kit
+ */
+class Test
+{
 
-	//@{ Reporting level
-	const
-		FLAG_False=0,
-		FLAG_True=1,
-		FLAG_Both=2;
-	//@}
+    //@{ Reporting level
+    const
+        FLAG_False = 0,
+        FLAG_True = 1,
+        FLAG_Both = 2;
+    //@}
 
-	protected
-		//! Test results
-		$data=[],
-		//! Success indicator
-		$passed=TRUE,
-		//! Reporting level
-		$level;
+    // Test results
+    protected array $data = [];
+    // Success indicator
+    protected bool $passed = true;
+    // Reporting level
+    protected int $level;
 
-	/**
-	*	Return test results
-	*	@return array
-	**/
-	function results() {
-		return $this->data;
-	}
+    /**
+     * Return test results
+     **/
+    public function results(): array
+    {
+        return $this->data;
+    }
 
-	/**
-	*	Return FALSE if at least one test case fails
-	*	@return bool
-	**/
-	function passed() {
-		return $this->passed;
-	}
+    /**
+     * Return FALSE if at least one test case fails
+     **/
+    public function passed(): bool
+    {
+        return $this->passed;
+    }
 
-	/**
-	*	Evaluate condition and save test result
-	*	@return object
-	*	@param $cond bool
-	*	@param $text string
-	**/
-	function expect($cond,$text=NULL) {
-		$out=(bool)$cond;
-		if ($this->level==$out || $this->level==self::FLAG_Both) {
-			$data=['status'=>$out,'text'=>$text,'source'=>NULL];
-			foreach (debug_backtrace(limit: 100) as $frame)
-				if (isset($frame['file'])) {
-					$data['source']=Base::instance()->
-						fixslashes($frame['file']).':'.$frame['line'];
-					break;
-				}
-			$this->data[]=$data;
-		}
-		if (!$out && $this->passed)
-			$this->passed=FALSE;
-		return $this;
-	}
+    /**
+     * Evaluate condition and save test result
+     */
+    public function expect(bool $cond, ?string $text = null): static
+    {
+        $out = $cond;
+        if ($this->level == $out || $this->level == self::FLAG_Both) {
+            $data = ['status' => $out, 'text' => $text, 'source' => null];
+            foreach (debug_backtrace(limit: 100) as $frame)
+                if (isset($frame['file'])) {
+                    $data['source'] =
+                        Base::instance()->fixslashes($frame['file']).':'.$frame['line'];
+                    break;
+                }
+            $this->data[] = $data;
+        }
+        if (!$out && $this->passed)
+            $this->passed = false;
+        return $this;
+    }
 
-	/**
-	*	Append message to test results
-	*	@return NULL
-	*	@param $text string
-	**/
-	function message($text) {
-		$this->expect(TRUE,$text);
-	}
+    /**
+     * Append message to test results
+     */
+    public function message(string $text): void
+    {
+        $this->expect(true, $text);
+    }
 
-	/**
-	*	Class constructor
-	*	@return NULL
-	*	@param $level int
-	**/
-	function __construct($level=self::FLAG_Both) {
-		$this->level=$level;
-	}
+    public function __construct(int $level = self::FLAG_Both)
+    {
+        $this->level = $level;
+    }
 
 }
